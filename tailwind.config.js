4bda4da8edcb0cdfa6b7d41c69f3a4425ba1c16e
leocs <leocsdias@hotmail.com>
2024-02-20 00:44:53 -0300

    created landing page sisamhil

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,15 +3,28 @@ module.exports = {
   content: ["index.html"],
   theme: {
     extend: {
+      backgroundImage: {
+        'hero': "url('/public/assets/img/elementos.png')",
+      },
       colors: {
         'primary': '#042F57',
-        'secondary': 'E4003C',
+        'secondary': '#E4003C',
         'background': '#F5F5F5',
         'background-foreground': '#DEE1E6',
       },
       fontFamily: {
-        body: ['Roboto'],
-      }
+        'roboto': ['Roboto', 'sans-serif'],
+        'quicksand': ['Quicksand', 'sans-serif'],
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: '1rem',
+          sm: '2rem',
+          lg: '4rem',
+          xl: '5rem',
+        }
+      },
     },
   },
   plugins: [],

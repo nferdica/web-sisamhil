edd1dfb7a181e163e9283eb682760db2bad0eacd
nferdica <ncaferme@gmail.com>
2024-02-19 23:34:24 -0300

    Upgrade project

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'hero': "url('/public/assets/img/elementos.png')",
+      },
+      colors: {
+        'primary': '#042F57',
+        'secondary': '#E4003C',
+        'background': '#F5F5F5',
+        'background-foreground': '#DEE1E6',
+      },
+      fontFamily: {
+        'roboto': ['Roboto', 'sans-serif'],
+        'quicksand': ['Quicksand', 'sans-serif'],
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: '1rem',
+          sm: '2rem',
+          lg: '4rem',
+          xl: '5rem',
+        }
+      },
+    },
+  },
+  plugins: [],
+}
+
